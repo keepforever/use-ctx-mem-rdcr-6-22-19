663122349921b7ca665268ecf5f7f892a65e1593
keepforever <briancilenti@gmail.com>
2019-06-23 10:27:49 -0400

    add onKeyDown to submit on 'Enter' press

--- src/comps/FormCustHook.js
+++ src/comps/FormCustHook.js
@@ -1,5 +1,5 @@
 import React, { useContext, useRef } from 'react';
-import {useForm} from '../hooks/useFormQuestion';
+import { useForm } from '../hooks/useFormQuestion';
 import PeopleContext from '../context/peopleContext';
 // styles
 import styles from '../styles';
@@ -14,39 +14,49 @@ const Form = () => {
     const submitPerson = async () => {
         console.log('\n', '\n', `values.name = `, values.name, '\n', '\n');
         if (!values.name.length) return;
-        context.addPerson({name: values.name});
+        context.addPerson({ name: values.name });
         clearForm();
         await getStuff();
     };
 
+    const handleKeyDown = e => {
+        if (e.key === 'Enter') {
+            submitPerson();
+        }
+    };
+
     return (
-        <div
-            style={styles['style-a']}
-        >
+        <div style={styles['style-a']}>
             <p>hello form</p>
             <input
                 type="text"
                 name="name"
+                onKeyDown={handleKeyDown}
                 placeholder="Enter name..."
                 value={values.name || ''}
                 onChange={handleChange}
                 style={styles['input']}
             />
-            <button
-                style={styles['input']}
-                onClick={submitPerson}
-            >
+            <button style={styles['input']} onClick={submitPerson}>
                 Submit Person
             </button>
-            {values.data && <div style={{
-                border: '2px solid green'
-            }}>
-                <h2 style={{
-                    textDecoration: values.data.completed ? 'line-through' : null
-                }}>
-                    {values.data.title}
-                </h2>
-            </div>}
+            {values.data && (
+                <div
+                    style={{
+                        border: '2px solid green'
+                    }}
+                >
+                    <h2
+                        style={{
+                            textDecoration: values.data.completed
+                                ? 'line-through'
+                                : null
+                        }}
+                    >
+                        {values.data.title}
+                    </h2>
+                </div>
+            )}
         </div>
     );
 };
